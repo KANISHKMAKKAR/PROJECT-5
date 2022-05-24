36579ddcdf594623cdb982889d7e3f82f29b54c1
KANISHKMAKKAR <kanishk.makkar.333@gmail.com>
2022-05-24 11:22:58 +0530

    Index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express= require('express')
+const bodyParser=require('body-parser')
+const app=express()
+
+const mongoose=require('mongoose')
+
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb+srv://functionup-uranium-cohort:q8znVj4ly0Fp0mpU@cluster0.0wdvo.mongodb.net/group46Database',
+{
+    useNewUrlParser:true
+})
+.then(()=>console.log('Mongo Db Is Connected'))
+.catch(err=>console.log(err))
+
+
+
+
+app.listen(3000,function(){
+    console.log('Running on Port 3000')
+})
