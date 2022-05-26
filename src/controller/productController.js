7011f19582d0f9c3cd9330ee26e9b1e0078a2bd1
sunilmsz <inpb91@gmail.com>
2022-05-26 21:35:42 +0530

    produectupdate api done

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,9 +1,10 @@
 let productModel = require('../model/productModel')
 let uploadFile = require('../controller/awsController')
-let { isValid, isvalidaddress, isvalidPincode, isValidPassword, isValidRequestBody, isValidfiles } = require('../validators/validator')
+let { isValid, isvalidaddress, isvalidPincode, isValidPassword, isValidRequestBody, isValidfiles ,isValid2,isBoolean} = require('../validators/validator')
 const currencySymbol = require("currency-symbol")
 const { default: mongoose } = require('mongoose')
 const { AppSync } = require('aws-sdk')
+
 
 let newProduct = async (req, res) => {
     try {
@@ -73,7 +74,7 @@ let getByIDProduct = async (req, res) => {
             return res.status(400).send({ status: false, message: "INVALID PRODUCT ID" });
         }
 
-        const product = await productModel.findOne({ _id: productId, isDeleted: false, deletedAt: null, });
+        const product = await productModel.findOne({ _id: productId, isDeleted: false, });//deletedAt: null,
 
         if (!product) {
             return res.status(404).send({ status: false, message: "NO PRODUCT FOUND BY THIS ID" })
@@ -81,36 +82,136 @@ let getByIDProduct = async (req, res) => {
         res.status(200).send({ status: true, message: "success", data: product });
     }
     catch (error) {
-        res.status(500).send({ status:false , message: error.message });
+        res.status(500).send({ status: false, message: error.message });
     }
 };
 
 
 
-let updateByIDProduct = (req, res) => {
+let updateByIDProduct = async (req, res) => {
+
+    const productId = req.params.productId;
+
+    if (!mongoose.Types.ObjectId.isValid(productId)) 
+        return res.status(400).send({ status: false, message: "INVALID PRODUCT ID" });
+
+    let requestBody = req.body
+    if (!isValidRequestBody(requestBody)) 
+        return  res.status(400).send({ status: false, Message: "Invalid request parameters, Please provide user details" })
+
+    const product ={}
+    let { title, description, price, currencyId, currencyFormat, isFreeShipping, style, availableSizes, installments ,productImage} = requestBody
+    let files = req.files
+    
+    if(productImage!=undefined)
+    return res.status(400).send({status:false,message:"Product Image should have image file"})
+
+    if (title || title=="") {
+        if (!isValid2(title))
+            return res.status(400).send({ status: false, message: "ADD A VALID TITLE" })
+        let duplicateTitle = await productModel.findOne({ title ,isDeleted:false })
+        if (duplicateTitle) {
+            return res.status(400).send({ status: false, message: "TITLE already present" })
+        }
+        product.title = title;
+    }
+
+    if(description || description=="")
+    {
+        if (!isValid2(description))
+        return res.status(400).send({ status: false, message: "ADD VALID DESCRIPTION" })
+        product.description = description;
+    }
+    
+    if(price)
+    {
+        if (isValid2(price) || !price.trim().match(/^[1-9]\d*(\.\d{1,2})?%?$/))
+        return res.status(400).send({ status: false, message: "ADD VALID PRICE " })
+        product.price=price;
+    }
+    
+    if(currencyId || currencyId=="" )
+    {
+        if (currencyId != "INR")
+        return res.status(400).send({ status: false, message: "ADD VALID CURRENCY, at this time only INR is acceptible" })
+        product.currencyId = currencyId;
+    }
+    
+    if(style || style =="")
+    {
+        if(!isValid2(style))
+        return res.status(400).send({ status: false, message:"please enter valid style"})
+        product.style=style
+    }
+
+    //currencyFormat = "₹"  as during creation we are not getting it user ,so user can't update that also
+
+    if (isFreeShipping || isFreeShipping=="") {
+        isFreeShipping = isBoolean(isFreeShipping)
+        if (isFreeShipping==="error")
+            return res.status(400).send({ status: false, message: "FREE SHIPPING MUST BE A BOOLEAN VALUE either true or false" })
+            product.isFreeShipping=isFreeShipping
+    }
+
+    if(installments|| installments==""){
+        const number = Math.floor(Number(installments))
+        if(isNaN(number) || number!=installments || installments<1)
+       return  res.status(400).send({status:false,message:`installments could have only a +ve number`})
+        product.installments = installments
+
+    }
+
+    if (isValidfiles(files))
+    {       
+        if (files.length > 1 || files[0].fieldname != "productImage")
+        return res.status(400).send({status:false,message:`Only One ProductImage is allowed by the field name productImage, no any other file or field allowed `}) 
+
+    if (!["image/png", "image/jpeg"].includes(files[0].mimetype))
+        return res.status(400).send({status:false,message:"only png,jpg,jpeg files are allowed from productImage"})
+
+        product.productImage = await uploadFile.uploadFile(files[0])
+    }
+    
+
+    if(availableSizes || availableSizes=="")
+    {
+        availableSizes = availableSizes.split(",")
+        for (let i = 0; i < availableSizes.length; i++) {
+            if (!["S", "XS", "M", "X", "L", "XXL", "XL"].includes(availableSizes[i]))
+                return res.status(400).send({ status: false, message: "AVAILABLE SIZE CAN BE S,XS,M,X,L,XXL,XL" })
+        }
+        product.availableSizes =availableSizes;
+    }
+    
+    const updatedProduct = await productModel.findOneAndUpdate({_id:productId,isDeleted:false},product,{new:true})
+
+    if(!updatedProduct)
+    return res.status(404).send({ status: false, message: "NO PRODUCT FOUND BY THIS ID" })
+
+    res.status(200).send({ status: true, message: "success", data: updatedProduct });
 
 }
-let deleteByIDProduct = async(req, res) => {
+let deleteByIDProduct = async (req, res) => {
     try {
         const productId = req.params.productId;
-        
-         if (!mongoose.Types.ObjectId.isValid(productId)) {
+
+        if (!mongoose.Types.ObjectId.isValid(productId)) {
             return res.status(400).send({ status: false, message: "NOT A VALID ID" })
         }
 
-        
-        const product = await productModel.findOne({ _id: productId, isDeleted: false, deletedAt: null});
+
+        const product = await productModel.findOne({ _id: productId, isDeleted: false, deletedAt: null });
 
         if (!product) {
-            return res.status(404).send({status: false, message: "NO PRODUCT FOUND"});
+            return res.status(404).send({ status: false, message: "NO PRODUCT FOUND" });
         }
-        
-        const DELETE = await productModel.findByIdAndUpdate(productId,{ $set:{ isDeleted:true, deletedAt:Date.now()}});
-       
-        res.status(200).send({ status: true, message:`PRODUCT WITH ID ${productId} DELETED` });
-    } 
+
+        const DELETE = await productModel.findByIdAndUpdate(productId, { $set: { isDeleted: true, deletedAt: Date.now() } });
+
+        res.status(200).send({ status: true, message: `PRODUCT WITH ID ${productId} DELETED` });
+    }
     catch (error) {
-        res.status(500).send({status:false,message: error.message });
+        res.status(500).send({ status: false, message: error.message });
     }
 }
 module.exports = { newProduct, getProducts, getByIDProduct, updateByIDProduct, deleteByIDProduct }
--- src/validators/validator.js
+++ src/validators/validator.js
@@ -3,6 +3,12 @@ const isValid = function (value) {
     if (typeof value === 'string' && value.trim().length === 0) return false
     return true;
 }
+
+const isValid2= (value)=> {
+    if (value.trim().length === 0 || !value.match(/[a-zA-Z]/)) return false // typeof value != 'string' 
+    return true;
+}
+
 let isvalidaddress = (value) => ({}.toString.call(value) == '[object Object]') ? true : false
 const isValidRequestBody = function (requestBody) {
     return Object.keys(requestBody).length > 0
@@ -16,4 +22,8 @@ const isValidfiles = function (files) {
     if (files && files.length > 0)
         return true
 }
-module.exports={isValid,isvalidaddress,isvalidPincode,isValidPassword,isValidRequestBody,isValidfiles}
+const isBoolean = (value) => {
+    value = value.toLowerCase()
+    return value==="true"?true:(value==="false")?false:"error"
+}
+module.exports={isValid,isvalidaddress,isvalidPincode,isValidPassword,isValidRequestBody,isValidfiles,isValid2,isBoolean}
